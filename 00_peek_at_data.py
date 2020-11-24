4291c8ab6951cb9037909a19eab39a152018b5df
ph1001 <54173557+ph1001@users.noreply.github.com>
2020-11-24 16:38:13 +0000

    Initial

---
+++ 00_peek_at_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 18:05:39 2020
+
+@author: philippmetzger
+"""
+
+#%% Import libaries
+import pandas as pd
+import os
+
+#%% Load data
+data = pd.read_csv(os.path.join('Project-20201106', 'donors.csv'), sep=',', index_col=0)
+
+#%% Testing debugging
+for i in range(10):
+    print('Vor breakpoint')
+    print(i)
+    print('Nach breakpoint')
